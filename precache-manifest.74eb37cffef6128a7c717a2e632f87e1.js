76240564f568db7324e165acc19013fd1d6b1117
Filipp <fili92@mail.ru>
2020-01-25 18:07:20 +0300

    Updates

---
+++ precache-manifest.74eb37cffef6128a7c717a2e632f87e1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a9cdad743b0396a6f00c3b1bf8fbf2",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "094c9e3a5d7ad88b1204",
+    "url": "/shopping/static/css/2.505afc3c.chunk.css"
+  },
+  {
+    "revision": "b5220e65fdc5b023425f",
+    "url": "/shopping/static/css/main.87bc7fc7.chunk.css"
+  },
+  {
+    "revision": "094c9e3a5d7ad88b1204",
+    "url": "/shopping/static/js/2.cbef3add.chunk.js"
+  },
+  {
+    "revision": "26f0e335d0c7337fc82a8966a2b29516",
+    "url": "/shopping/static/js/2.cbef3add.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5220e65fdc5b023425f",
+    "url": "/shopping/static/js/main.8b21994f.chunk.js"
+  },
+  {
+    "revision": "8d8dfed2bc0832e5dbbf",
+    "url": "/shopping/static/js/runtime-main.775506c5.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/shopping/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/shopping/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/shopping/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/shopping/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/shopping/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/shopping/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/shopping/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/shopping/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/shopping/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/shopping/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/shopping/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/shopping/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/shopping/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/shopping/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/shopping/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/shopping/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
